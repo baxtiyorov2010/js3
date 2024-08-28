d8041ee706369556bc496ad1b367bc67aa515b43
baxtiyorov2010 <torabaxtiyorov29@gmail.com>
2024-08-28 15:20:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("siga random son chiqar beradi")
+let number = Math.floor(Math.random() * 100)
+alert("random son: " + number)
+let a = number * 2
+let b = number % 2
+let c = number % 3
+alert(`
+    random orqali chiqan son ${number} edi 
+    biz uni 2ga kopaytirganda  ${a} shu  son chiqdi 
+    va shu soni 2ga bolganda ${b} shu son chiqdi
+    va uni 3ga bolganda ,  ${c} shu son chiqdi
+     `)
